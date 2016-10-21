345015c643aac824eef6fce0c3498a03071c8711
franmartosr <franciscomr1990@gmail.com>
2016-10-21 10:04:50 +0200

    [MODIFY] Optimized call to render function in single lang view

--- src/js/mvc/views/singleLang.js
+++ src/js/mvc/views/singleLang.js
@@ -11,11 +11,7 @@ define([
     template: _.template(tmpl),
     initialize: function(params) {
       this.model.url = `language/${params.language}`;
-      this.model.fetch({
-        success: function() {
-          this.render();
-        }.bind(this)
-      });
+      this.model.fetch({ success: this.render.bind(this) });
     },
     render: function() {
       this.$el.html(this.template({ data: this.model.attributes }));
